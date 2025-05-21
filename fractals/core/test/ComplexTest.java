2f7f860911d47697ce34cc24633d045bde6b8864
Konstantin Gerks <konstantin.gerks@stud.tu-darmstadt.de>
2025-05-21 21:09:28 +0200

    Improve unit test numbers of Complex class

--- fractals/core/test/ComplexTest.java
+++ fractals/core/test/ComplexTest.java
@@ -9,44 +9,44 @@ public class ComplexTest
     @Test
     void addTest()
     {
-        Complex c1 = new Complex(1.0, 1.0);
-        Complex c2 = new Complex(2.0, 2.0);
+        Complex c1 = new Complex(1.0, 2.0);
+        Complex c2 = new Complex(3.0, 4.0);
         Complex c3 = c1.add(c2);
-        assertEquals(3.0, c3.getReal());
-        assertEquals(3.0, c3.getImag());
+        assertEquals(4.0, c3.getReal());
+        assertEquals(6.0, c3.getImag());
     }
 
     @Test
     void subTest()
     {
-        Complex c1 = new Complex(3.0, 3.0);
-        Complex c2 = new Complex(2.0, 2.0);
+        Complex c1 = new Complex(6.0, 8.0);
+        Complex c2 = new Complex(3.0, 4.0);
         Complex c3 = c1.sub(c2);
-        assertEquals(1.0, c3.getReal());
-        assertEquals(1.0, c3.getImag());
+        assertEquals(3.0, c3.getReal());
+        assertEquals(4.0, c3.getImag());
     }
 
     @Test
     void multTest()
     {
-        Complex c1 = new Complex(4.0, 1.0);
-        Complex c2 = new Complex(3.0, 2.0);
+        Complex c1 = new Complex(4.0, 3.0);
+        Complex c2 = new Complex(5.0, 2.0);
         Complex c3 = c1.mult(c2);
-        assertEquals(10.0, c3.getReal());
-        assertEquals(11.0, c3.getImag());
+        assertEquals(14.0, c3.getReal());
+        assertEquals(23.0, c3.getImag());
     }
 
     @Test
     void normTest()
     {
-        Complex c = new Complex(2.0, 2.0);
-        assertEquals(Math.sqrt(8), c.norm());
+        Complex c = new Complex(3.0, 4.0);
+        assertEquals(5.0, c.norm());
     }
 
     @Test
     void cloneTest()
     {
-        Complex c1 = new Complex(3.0, 3.0);
+        Complex c1 = new Complex(2.0, 3.0);
         Complex c2 = c1.clone();
         assertEquals(c1.getReal(), c2.getReal());
         assertEquals(c1.getImag(), c2.getImag());
@@ -56,8 +56,8 @@ public class ComplexTest
     @Test
     void toStringTest()
     {
-        Complex c = new Complex(2.0, 2.0);
-        assertEquals("2.0 + 2.0i", c.toString());
+        Complex c = new Complex(4.0, 5.0);
+        assertEquals("4.0 + 5.0i", c.toString());
     }
 
 }
